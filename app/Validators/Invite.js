a1abd8cff916eb271f7dd05476285d57736c6aaf
mtheusbrito <matheusbriodasilva@gmail.com>
2019-12-10 14:26:24 -0300

    validacoes

---
+++ app/Validators/Invite.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class Invite {
+  get validateAll(){
+    return true;
+  }
+  get rules () {
+    return {
+      // validation rules
+   invites: 'required|array',
+   'invites.*' : 'required|email'
+    }
+  }
+}
+
+module.exports = Invite
---
+++ app/Validators/Session.js
@@ -0,0 +1,15 @@
+'use strict'
+
+class Session {
+  get validateAll(){
+    return true;
+  }
+  get rules () {
+    return {
+      email : 'required',
+      password: 'required'
+    }
+  }
+}
+
+module.exports = Session
